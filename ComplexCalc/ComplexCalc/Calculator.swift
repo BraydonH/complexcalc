09f352e6e297127c79bfeaafd742f783a34e5b5d
Braydon Hall <BraydonH@cs.washington.edu>
2019-01-17 15:39:57 -0800

    simple tests passing

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,28 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func add(_ nums: [Int]) -> Int {
+        var sum = 0
+        for x in nums {
+            sum += x
+        }
+        return sum
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
 }
